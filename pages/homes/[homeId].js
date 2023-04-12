b12dd3f80f7a355e4599b26f48c757c7f0e2f82c
Donovan Rivers <donovanriversjr@gmail.com>
2023-04-12 18:12:24 -0400

    Finished Project

--- pages/homes/[homeId].js
+++ pages/homes/[homeId].js
@@ -21,7 +21,6 @@ import Avatar from '@mui/material/Avatar';
 import dayjs from 'dayjs';
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
-import Backdrop from '@mui/material/Backdrop';
 import Image from "next/image"
 import { useRouter } from "next/router";
 import 'flowbite';
@@ -36,21 +35,124 @@ function houseOne(){
 }
 
 
+
 function HouseOne(){
-  
+
+  const [openReserveBtn, setOpenReserveBtn] = React.useState(false)
+  const [openMinNightsModal, setOpenMinNightsModal] = React.useState(false)
+  const [openCleaningFeeModal, setOpenCleaningFeeModal] = React.useState(false)
+  const [openServiceFeeModal, setOpenServiceFeeModal] = React.useState(false)
+  const [openThisSpace, setOpenFive] = React.useState(false)
+
+  const handleOpenReserveBtn = () => setOpenReserveBtn(true)
+  const handleOpenMinNightsModal = () => setOpenMinNightsModal(true)
+  const handleOpenCleaningFeeModal = () => setOpenCleaningFeeModal(true)
+  const handleOpenServiceFeeModal = () => setOpenServiceFeeModal(true)
+  const handleOpenThisSpace = () => setOpenFive(true)
+
+  const handleCloseReserveBtn = () => setOpenReserveBtn(false)
+  const handleCloseMinNightsModal = () => setOpenMinNightsModal(false)
+  const handleCloseCleaningFeeModal = () => setOpenCleaningFeeModal(false)
+  const handleCloseServiceFeeModal = () => setOpenServiceFeeModal(false)
+  const handleCloseThisSpace = () => setOpenFive(false)
+
   const [data, setData] = useState([])
-  const [open, setOpen] = React.useState(false);
   const router = useRouter()
   const homeId = router.query.homeId
-  const url = "https://planets.pythonanywhere.com/houses/" + homeId + "/"
 
-  const handleClose = () => {
-    setOpen(false);
-  };
-  const handleToggle = () => {
-    setOpen(!open);
-  };
-  useEffect(() =>{if (router.isReady) (fetch(url).then((response) => response.json()).then((data) => setData(data)))},[router.isReady])
+  useEffect(() =>{if (router.isReady) (fetch(`https://planets.pythonanywhere.com/houses/${homeId}/`).then((response) => response.json()).then((data) => setData(data)))},[router.isReady])
+
+  
+  let timesFive = data.minNightsFee * 5
+  let minNightTimesFive = timesFive.toLocaleString()
+
+  let svFee = data.serviceFee * 1
+  let serveFee = data.serviceFee
+  let serviceFeeComma = svFee.toLocaleString()
+
+  let cleanFee = data.cleaningFee 
+  
+  let total = Number(serveFee) + Number(timesFive) + Number(cleanFee)
+
+  let totalbeforeTax = total.toLocaleString()
+
+
+const srcset = (image, width, height, rows = 1, cols = 1) =>{
+    return {
+      src: `${image}?w=${width * cols}&h=${height * rows}&fit=crop&auto=format`,
+      srcSet: `${image}?w=${width * cols}&h=${
+        height * rows
+      }&fit=crop&auto=format&dpr=2 2x`,
+    };
+}
+  
+const StandardImageList = () =>{
+    return (
+      <ImageList
+        sx={{
+          maxWidth: 900,
+          maxHeight: 650,
+          minWidth: 350,
+          height: 'auto',
+          transform: 'translateZ(0)',
+          paddingTop: '5px',
+          paddingBottom: '5px',
+          borderRadius: '20px'
+          
+        }}
+        rowHeight={350}
+        gap={3}
+      >
+        {itemData.map((item) => {
+          const cols = item.featured ? 2 : 1;
+          const rows = item.featured ? 2 : 1;
+  
+          return (
+            <ImageListItem key={item.img} cols={cols} rows={rows}>
+              <img {...srcset(item.img, 250, 200, rows, cols)} alt={item.title} loading="lazy" />
+              <ImageListItemBar sx={{background:'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' + 'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)', }} title={item.title} position="top"/></ImageListItem>
+          );
+        })}
+      </ImageList>
+    );
+}
+  
+const HousePhotosModal = () => {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+  
+    return(
+      <div className="overflow-hidden">
+        <Button className=" font-normal flex" onClick={handleOpen} theme={theme} color="primary" sx={{textTransform: "none"}}><Image src="/icons/icons8-grid-view-22.png" className="" width={22} maxWidth={22} height={22}/>Show all photos</Button>
+        <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title">
+          <Box elevation={0} sx={pictureBox}>
+            <div className="grid grid-cols-1 divide-y overflow-hidden">
+            <StandardImageList/>
+            </div>
+          </Box>
+        </Modal>
+      </div>
+    )
+}
+
+const itemData = [
+    {
+      img: data.home_img,
+      featured: true,
+    },
+    {
+      img: data.listImgOneUrl,
+    },
+    {
+      img: data.listImgTwoUrl,
+    },
+    {
+      img: data.listImgThreeUrl,
+      featured: true,
+    }
+]
+
     {
       return(
         <div>
@@ -99,7 +201,19 @@ function HouseOne(){
         </Stack>
         <Divider sx={{color: '#a9a9a9', backgroundColor: 'a9a9a9', marginTop: '20px', marginBottom: '20px'}} orientation="horizontal" flexItem/>
         <Paper elevation={0} sx={{width: '100%', maxWidth: '1000px', height: 'auto', margin: 'auto'}}>
-        <div className="text-medium text-center max-w-lg	m-auto ">{data.description}...<div className="text-medium text-decoration-line: underline"><FullHomeDescription/></div></div>
+        <div className="text-medium text-center max-w-lg	m-auto ">{data.description}...<div className="text-medium text-decoration-line: underline"><div>
+        <button onClick={handleOpenThisSpace} theme={theme} color="primary" className="p-2 text-decoration-line: underline text-lg">See More<ArrowForwardIosIcon/></button>
+        <Modal open={openThisSpace} onClose={handleCloseThisSpace} aria-labelledby="modal-modal-title" >
+          <Box sx={descriptionStyle}>
+            <div className="grid grid-cols-1 space-y-4">
+            <div className="text-left text-2xl font-semibold" sx={{ mt: 2 }}>About this space</div>
+            <div className="text-left" sx={{ mt: 2 }}>{data.aboutDescription}</div>
+            <div className="text-left text-xl font-semibold" sx={{ mt: 2}}>The space</div>
+            <div className="text-left text-medium" sx={{ mt: 2 }}>{data.theSpace}</div>
+            </div>
+          </Box>
+        </Modal>
+      </div></div></div>
         <Divider sx={{color: '#a9a9a9', backgroundColor: 'a9a9a9', marginTop: '20px', marginBottom: '20px'}} orientation="horizontal" flexItem/>
         </Paper>
         </div>
@@ -117,44 +231,98 @@ function HouseOne(){
         </div> 
         </div>
         <Paper elevation={2} sx={{width: '100%', maxWidth: '350px', height: 'auto'}}  className="text-center text-white py-2 px-4 space-y-4 ">
-        <div className="space-y-4 ">
-        <div>
-        <Stack direction="row" justifyContent="center" alignItems="center" spacing={2}>
+        <div className="space-y-2 ">
+        <div className="">
+        <Stack direction="row" justifyContent="center" alignItems="center" spacing={10}>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DatePicker label="Start Date" id="start_date" disablePast={true}  className="pb-5" />
         <DatePicker label="End Date"  id="end_date" disablePast={true}  minDate={tomorrow} className="pb-5" />
         </LocalizationProvider>
         </Stack>
-        <button onClick={handleToggle} className="text-white bg-gradient-to-r from-red-500 to-pink-500 hover:bg-gradient-to-br rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 w-80">Reserve</button>
-        <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open={open} onClick={handleClose}>
+        <button onClick={handleOpenReserveBtn} className="text-white bg-gradient-to-r from-red-500 to-pink-500 hover:bg-gradient-to-br rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 w-80">Reserve</button>
+        <Modal open={openReserveBtn} onClose={handleCloseReserveBtn} aria-labelledby="modal-modal-title" >
         <Box sx={style} >
         <div className="space-y-6">
         <div className="text-center text-black" sx={{ mt: 2 }}>{data.title} Booked!</div>
         </div>
         </Box>
-        </Backdrop>
+        </Modal>
         </div>
         <div className="text-center font-small text-stone-800 ">Fees</div>
         </div>
-        <div className="flex space-x-28">
-        <div className="text-black font-light	text-center text-decoration-line: underline"><NightFeesModal/></div>
-        <div className="text-black text-medium">$7,013</div>
+        <div className="">
+
+        <div className="text-black font-light	 text-decoration-line: underline" >
+        <Stack direction="row" justifyContent="center" alignItems="center" spacing={7}>
+
+        <button className="text-decoration-line: underline" style={{paddingRight: '50px'}} onClick={handleOpenMinNightsModal} theme={theme} color="neutral">${data.minNightsFee} x 5 nights</button>
+        <Modal open={openMinNightsModal} onClose={handleCloseMinNightsModal} aria-labelledby="modal-modal-title" >
+          <Box sx={style} >
+            <div className="space-y-6">
+            <div className="text-center" sx={{ mt: 2 }}>Minimum Nights: ${minNightTimesFive}
+            <div className="text-center" sx={{ mt: 2 }}>Homes are able to declare a minimum stay on their homes. The minimum night fee is the base per night fee, multplied by the specified minimum number of nights.</div></div>
+            </div>
+          </Box>
+        </Modal>
+        <div className="text-black text-medium ">${minNightTimesFive}</div>
+
+        </Stack>
+        </div>        
         </div>
-        <div className="flex space-x-40">
-        <div className="text-black	text-center text-decoration-line: underline"><CleaningFeesModal/></div>
-        <div className="text-black text-medium">$375</div>
-        </div><div className="flex space-x-40">
-        <div className="text-black font-light	text-center text-decoration-line: underline"><ServiceFeeModal/></div>
-        <div className="text-black text-medium">$1,043</div>
+        <div className="text-black text-decoration-line: underline" ><div>
+        <Stack direction="row" justifyContent="center" alignItems="center" spacing={7} >
+
+        <button className="text-decoration-line: underline" style={{paddingRight: '90px'}} onClick={handleOpenCleaningFeeModal} theme={theme} color="neutral">Cleaning Fee</button>
+        <Modal open={openCleaningFeeModal} onClose={handleCloseCleaningFeeModal} aria-labelledby="modal-modal-title" >
+        <Box sx={style} >
+          <div className="space-y-6">
+          <div className="text-center" sx={{ mt: 2 }}>Cleaning Fee: ${data.cleaningFee}
+          <div className="text-center" sx={{ mt: 2 }}>One-time fee charged by host to cover the cost of cleaning their space.</div></div>
+          </div>
+        </Box>
+        </Modal>
+        <div className="text-black text-medium">${data.cleaningFee}</div>
+        
+        </Stack>
+       </div>
+       
         </div>
+
+        <div className="text-black font-light text-decoration-line: underline"><div>
+        <Stack direction="row" justifyContent="center" alignItems="center" spacing={7}>
+
+        <button className="text-decoration-line: underline" style={{paddingRight: '90px'}} onClick={handleOpenServiceFeeModal} theme={theme} color="neutral">Service Fee</button>
+        <Modal open={openServiceFeeModal} onClose={handleCloseServiceFeeModal} aria-labelledby="modal-modal-title" >
+        <Box sx={style} >
+          <div className="space-y-6">
+          <div className="text-center" sx={{ mt: 2 }}>Service Fee: ${serviceFeeComma}
+          <div className="text-center" sx={{ mt: 2 }}>This helps us run our platform and offer services like 24/7 support on your trip.</div></div>
+          </div>
+        </Box>
+      </Modal>
+      
+      <div className="text-black text-medium">${serviceFeeComma}</div>
+      </Stack>
+
+      </div>
+      
+        </div>
+        
         <Divider sx={{color: '#a9a9a9', backgroundColor: 'a9a9a9'}} orientation="horizontal" flexItem/>
+        <Stack direction="row" justifyContent="center" alignItems="center" spacing={7}>
+
         <div className="flex">
         <div className="text-black text-center font-semibold">Total before taxes</div>
-        <div className="text-black font-semibold" style={{marginLeft: '105px'}}>$8,431</div>
+        <div className="text-black 	text-right font-semibold" style={{marginLeft: '95px'}}>${totalbeforeTax}</div>
         </div>
-        </Paper>
-        </Grid>
         </Stack>
+
+        </Paper>
+        
+        </Grid>
+        
+        </Stack>
+        
         </div>  
 
       )  
@@ -210,133 +378,9 @@ const pictureBox = {
   height: 'auto',
   p: 4, 
 }
-const itemData = [
-  {
-    img: '/houseOne/homeOne.webp',
-    title: 'Exterior',
-    featured: true,
 
-
-  },
-  {
-    img: '/houseOne/1.webp',
-    title: 'Overhead View',
-
-  },
-  {
-    img: '/houseOne/2.webp',
-    title: 'Lake View',
-
-  },
-  {
-    img: '/houseOne/3.webp',
-    title: 'Lake View 2',
-    featured: true,
-    
-  }
-]
 const tomorrow = dayjs().add(1, 'day');
 
-
-const srcset = (image, width, height, rows = 1, cols = 1) =>{
-  return {
-    src: `${image}?w=${width * cols}&h=${height * rows}&fit=crop&auto=format`,
-    srcSet: `${image}?w=${width * cols}&h=${
-      height * rows
-    }&fit=crop&auto=format&dpr=2 2x`,
-  };
-}
-
-const StandardImageList = () =>{
-  return (
-    <ImageList
-      sx={{
-        maxWidth: 900,
-        maxHeight: 650,
-        minWidth: 350,
-        height: 'auto',
-        transform: 'translateZ(0)',
-        paddingTop: '5px',
-        paddingBottom: '5px',
-        borderRadius: '20px'
-        
-      }}
-      rowHeight={350}
-      gap={3}
-    >
-      {itemData.map((item) => {
-        const cols = item.featured ? 2 : 1;
-        const rows = item.featured ? 2 : 1;
-
-        return (
-          <ImageListItem key={item.img} cols={cols} rows={rows}>
-            <img {...srcset(item.img, 250, 200, rows, cols)} alt={item.title} loading="lazy" />
-            <ImageListItemBar sx={{background:'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' + 'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)', }} title={item.title} position="top"/></ImageListItem>
-        );
-      })}
-    </ImageList>
-  );
-}
-
-const NightFeesModal = () => {
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-  
-    return (
-      <div>
-        <button className="text-decoration-line: underline" onClick={handleOpen} theme={theme} color="neutral">$1,409 x 5 nights</button>
-        <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" >
-          <Box sx={style} >
-            <div className="space-y-6">
-            <div className="text-center" sx={{ mt: 2 }}>Minimum Nights: $7,013
-            <div className="text-center" sx={{ mt: 2 }}>Homes are able to declare a minimum stay on their homes. The minimum night fee is the base per night fee, multplied by the specified minimum number of nights.</div></div>
-            </div>
-          </Box>
-        </Modal>
-      </div>
-    );
-}  
-
-const CleaningFeesModal = () =>{
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
-  return (
-    <div>
-      <button className="text-decoration-line: underline" onClick={handleOpen} theme={theme} color="neutral">Cleaning Fee</button>
-      <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" >
-        <Box sx={style} >
-          <div className="space-y-6">
-          <div className="text-center" sx={{ mt: 2 }}>Cleaning Fee: $375
-          <div className="text-center" sx={{ mt: 2 }}>One-time fee charged by host to cover the cost of cleaning their space.</div></div>
-          </div>
-        </Box>
-      </Modal>
-    </div>
-  );
-}
-
-const ServiceFeeModal = () =>{
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
-  return (
-    <div>
-      <button className="text-decoration-line: underline" onClick={handleOpen} theme={theme} color="neutral">Service Fee</button>
-      <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" >
-        <Box sx={style} >
-          <div className="space-y-6">
-          <div className="text-center" sx={{ mt: 2 }}>Cleaning Fee: $1,043
-          <div className="text-center" sx={{ mt: 2 }}>This helps us run our platform and offer services like 24/7 support on your trip.</div></div>
-          </div>
-        </Box>
-      </Modal>
-    </div>
-  );
-}
 
 const ReviewsModal = () =>{
   const [open, setOpen] = React.useState(false);
@@ -358,47 +402,6 @@ const ReviewsModal = () =>{
       </Modal>
     </div>
   );
-}
-
-const HousePhotosModal = () => {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
-  return(
-    <div className="overflow-hidden">
-      <Button className=" font-normal flex" onClick={handleOpen} theme={theme} color="primary" sx={{textTransform: "none"}}><Image src="/icons/icons8-grid-view-22.png" className="" width={22} maxWidth={22} height={22}/>Show all photos</Button>
-      <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title">
-        <Box elevation={0} sx={pictureBox}>
-          <div className="grid grid-cols-1 divide-y overflow-hidden">
-          <StandardImageList/>
-          </div>
-        </Box>
-      </Modal>
-    </div>
-  )
-}
-
-const FullHomeDescription = () =>{
-  const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-  
-    return (
-      <div>
-        <button onClick={handleOpen} theme={theme} color="primary" className="p-2 text-decoration-line: underline text-lg">See More<ArrowForwardIosIcon/></button>
-        <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" >
-          <Box sx={descriptionStyle}>
-            <div className="grid grid-cols-1 space-y-4">
-            <div className="text-left text-2xl font-semibold" sx={{ mt: 2 }}>About this space</div>
-            <div className="text-left" sx={{ mt: 2 }}>Reverse pandemic blues at this retreat charged 4 bedroom haven with an authentic Finnish sauna, canal-front jacuzzi consciousness, paddleboards, a tandem kayak, replete comfy zones to lazily exchange conversation. The tranquility of the canal will replace all Debbie downer thoughts with a symphony of luminous relaxation, bound to bedazzle wary minds and hearts.</div>
-            <div className="text-left text-xl font-semibold" sx={{ mt: 2}}>The space</div>
-            <div className="text-left text-medium" sx={{ mt: 2 }}>Tastefully furnished on the inside, this villa features 4 bedrooms with king or queen size beds, 2.5 bathrooms, two tuned grand pianos for dueling it out, and within a few minute paddle to the bay. After a few hours here you’ll forget about any invisible enemy that has destroyed the basic hug! Great for team building and corporate or family retreats! No parties allowed. Long-term rental an option.</div>
-            </div>
-          </Box>
-        </Modal>
-      </div>
-    );
 }
 
 export default houseOne
